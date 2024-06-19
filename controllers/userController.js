f37f12e474b55688f02f34e91dfe1d2cc19c1d25
Seu Nome <rafaelsantoscostat@gmail.com>
2024-06-19 12:15:25 -0300

    Autenticação via token

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,6 +35,54 @@ const register  =  async (req,res) => {
     
 }
 
+function checkToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) return res.status(401).json({ msg: "Acesso negado!" });
+
+  try {
+    const secret = process.env.SECRET;
+
+    jwt.verify(token, secret);
+
+    next();
+  } catch (err) {
+    res.status(400).json({ msg: "O Token é inválido!" });
+  }
+}
+
+const login  = async (req,res) => {
+    const {email, password} = req.body;
+
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res.status(404).json({ msg: "Usuário não encontrado!" });
+    }
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if (!checkPassword) {
+      return res.status(422).json({ msg: "Senha inválida" });
+    }
+  
+
+    try {
+      const secret = process.env.SECRET;
+  
+      const token = jwt.sign(
+        {
+          id: user._id,
+        },
+        secret
+      );
+  
+      res.status(200).json({ msg: "Autenticação realizada com sucesso!", token });
+    } catch (error) {
+      res.status(500).json({ msg: error });
+    }
+}
+
 
 module.exports = {
     register
